bb4bbe827e5a16923b046c9e2992e4f52996e54a
AdityaUndurti <aditya.undurti@outlook.com>
2015-05-14 17:53:23 -0700

    Candidate solution for Dijkstra

--- Dijkstra/Dijkstra/main.cpp
+++ Dijkstra/Dijkstra/main.cpp
@@ -3,48 +3,168 @@
 #include <string>
 #include <algorithm>
 
-bool ReduceToI(std::string c)
+struct element
 {
-	// sort, so it's always in the form of iiijjjkk, i.e. with all the is in the beginning, js in the middle and ks at the end
-	std::sort(c.begin(), c.end());
+	int sign;
+	char value;
+};
+
+
+element Multiply(element a, element b)
+{
+	element c;
+
+	if (a.value == '\0')
+	{
+		c.value = b.value;
+		c.sign = a.sign*b.sign;
+	}
+
+	if (b.value == '\0')
+	{
+		c.value = a.value;
+		c.sign = a.sign*b.sign;
+	}
+
+	// cases where a == b (i,j,k)
+	if (a.value == b.value)
+	{
+		c.value = '\0';
+		c.sign = -1 * a.sign * b.sign;		
+	}
+	
+	if (a.value == 'i' && b.value == 'j')
+	{
+		c.value = 'k';
+		c.sign = a.sign * b.sign;
+	}
+
+	if (a.value == 'i' && b.value == 'k')
+	{
+		c.value = 'j';
+		c.sign = -1 * a.sign * b.sign;
+	}
+
+	if (a.value == 'j' && b.value == 'k')
+	{
+		c.value = 'i';
+		c.sign = a.sign * b.sign;
+	}
+
+	if (a.value == 'j' && b.value == 'i')
+	{
+		c.value = 'k';
+		c.sign = -1 * a.sign * b.sign;
+	}
+
+	if (a.value == 'k' && b.value == 'i')
+	{
+		c.value = 'j';
+		c.sign = a.sign * b.sign;
+	}
+
+	if (a.value == 'k' && b.value == 'j')
+	{
+		c.value = 'i';
+		c.sign = -1 * a.sign * b.sign;
+	}
+
+	return c;
+}
+
+element Reduce(std::string c)
+{
+	element reduced;
+	reduced.sign = 1;
+	reduced.value = '\0';
+
+	for (size_t i = 0; i < c.size(); i++)
+	{
+		element current;
+		current.sign = 1;
+		current.value = c[i];
+
+		reduced = Multiply(reduced, current);
+	}
+	
+	return reduced;
+}
+
+int ReduceToI(std::string c)
+{	
 	std::cout << "i string: " << c << std::endl;
-	return false;
+
+	element reduced = Reduce(c);
+
+	if (reduced.value == 'i')
+	{
+		return reduced.sign;
+	}
+	
+	return 0;
 }
 
-bool ReduceToJ(std::string c)
+
+int ReduceToJ(std::string c)
 {
-	std::sort(c.begin(), c.end());
 	std::cout << "j string: " << c << std::endl;
-	return true;
+
+	element reduced = Reduce(c);
+	
+	if (reduced.value == 'j')
+	{
+		return reduced.sign;
+	}
+
+	return 0;
 }
 
-bool ReduceToK(std::string c)
+int ReduceToK(std::string c)
 {
-	std::sort(c.begin(), c.end());
 	std::cout << "k string: " << c << std::endl;
-	return true;
+
+	element reduced = Reduce(c);
+
+	if (reduced.value == 'k')
+	{
+		return reduced.sign;
+	}
+
+	return 0;
 }
 
 bool Solve(std::string c)
 {
-	for (size_t i = 0; i < c.size() - 2; i++)
+	for (size_t i = 1; i < c.size() - 2; i++)
 	{
 		std::string iString = c.substr(0, i);
-		if (ReduceToI(iString))
+
+		int signi = ReduceToI(iString);
+
+		if (signi != 0)
 		{
-			for (size_t j = i + 1; j < c.size() - 1; j++)
+			for (size_t j = i; j < c.size() - 1; j++)
 			{
-				std::string jString = c.substr(i + 1, j);
+				std::string jString = c.substr(i + 1, j - i - 1);
 
-				if (ReduceToJ(jString))
+				int signj = ReduceToJ(jString);
+
+				if (signj != 0)
 				{
-					for (size_t k = j + 1; k < c.size(); k++)
+					for (size_t k = j; k < c.size(); k++)
 					{
-						std::string kString = c.substr(i + 1, j);
+						std::string kString = c.substr(j + 1, k - j - 1);
 
-						if (ReduceToK(kString))
+						int signk = ReduceToK(kString);
+
+						if (signk != 0)
 						{
-							return true;
+							std::cout << "Checking for sign..." << std::endl;
+
+							if (signi * signj * signk == 1)
+							{								
+								return true;
+							}
 						}
 
 					}
@@ -55,14 +175,6 @@ bool Solve(std::string c)
 	}
 
 	return false;
-}
-
-
-char Multiply(char a, char b)
-{	
-	
-
-	return '\0';
 }
 
 int main()
