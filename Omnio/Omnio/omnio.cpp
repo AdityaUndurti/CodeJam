3976d45738644aecd8840bb66608fcf7cc640261
AdityaUndurti <aditya.undurti@outlook.com>
2015-05-23 21:47:48 -0700

    Final solution for large problem

--- Omnio/Omnio/omnio.cpp
+++ Omnio/Omnio/omnio.cpp
@@ -31,12 +31,8 @@ bool Solve(int X, int R, int C)
 		{
 			return false;
 		}
-	}
-	else if (X >= 7)
-	{
-		return true;
-	}
-	else
+	}	
+	else if (X == 4)
 	{
 		if (X >= R*C)
 		{
@@ -54,6 +50,53 @@ bool Solve(int X, int R, int C)
 		}
 
 		return false;
+	}
+	else if (X == 5)
+	{
+		if (X >= R*C)
+		{
+			return true;
+		}
+
+		if (R*C % X != 0)
+		{
+			return true;
+		}
+
+		if (R <= 2 || C <= 2)
+		{
+			return true;
+		}
+
+		if (R == 3 && C == 5 || C == 3 && R == 5)
+		{
+			return true;
+		}
+
+		return false;
+	}
+	else if (X == 6)
+	{
+		if (X >= R*C)
+		{
+			return true;
+		}
+
+		if (R*C % X != 0)
+		{
+			return true;
+		}
+
+		if (R <= 3 || C <= 3)
+		{
+			return true;
+		}
+
+		return false;
+	}
+	else
+	{
+		return true;
 	}
 
 }
